c263561ba756a7a3b5c48b87addecbdd64e809fb
Sara-Maged <83797445+Sara-Maged@users.noreply.github.com>
2021-09-15 11:43:21 +0200

    Update game-javascript.js

--- game-javascript.js
+++ game-javascript.js
@@ -4,23 +4,6 @@ let level = 5, winner = 50;
 function startGame(){
     randomGenerator();
     insertionExtra();
-
-    // if(level == winner){
-    //     $("img").remove();
-    //     document.getElementById("gameEnv").appendChild(<h1>YOU WIN</h1>)
-        
-    // }
-
-    // if(level == winner){
-    //     $("#body").append("<h1> YOU WIN !</h1>");
-    //     return $("#").css({
-    //         "position": "absolute",
-    //             "top": top + "%",
-    //             "left": left + "%"
-    //     });
-
-    //     break;
-    // }
 };
 
     function randomGenerator(){
@@ -28,13 +11,8 @@ function startGame(){
             Left = Math.floor(Math.random() * 80) + 10;
             Top = Math.floor(Math.random() * 80) + 5;
 
-            // Leftx = Math.floor(Math.random() * 80) + 10;
-            // Topx = Math.floor(Math.random() * 80) + 5;
-
             insertionR(Left, Top, i);
             insertionL(Left, Top, i);
-
-            //if(i > (min/5)) insertionExtra(Leftx, Topx, i);
 
     }
     
@@ -59,14 +37,6 @@ function insertionR(left, top, iteratorR){
         });
 };
 
-// for(var j = 0; j < 2; j++){
-
-//     Left = Math.floor(Math.random() * 80) + 10;
-//     Top = Math.floor(Math.random() * 80) + 5;
-
-//     insertionExtra(Left, Top, j);
-// }
-
 
 function insertionExtra(iterationExtra){
     $("#R").append("<img src = 'smiley.png' width='50' height='50' onclick='next()' id=" + iterationExtra + ">");
@@ -78,96 +48,10 @@ function insertionExtra(iterationExtra){
 };
 
 function next(){
-    // document.getElementById(iterationExtra)
 
     level+=5;
     $("img").remove();
     startGame(level);
 
     if(level > winner) alert("YOU WIN");
-
-    // for(var x = 0; x < level; x++){
-    //     var element = document.getElementById("x").style.display='none';
-	    
-    // }
-
-
-
 }
-
-
-
-
-// (function makeDiv(){
-//     // vary size for fun
-//     var divsize = ((Math.random()*100) + 50).toFixed();
-//     var color = '#'+ Math.round(0xffffff * Math.random()).toString(16);
-//     $newdiv = $('<div/>').css({
-//         'width':divsize+'px',
-//         'height':divsize+'px',
-//         'background-color': color
-//     });
-
-//     // make position sensitive to size and document's width
-//     var posx = (Math.random()).toFixed();
-//     var posy = (Math.random()).toFixed();
-
-//     $newdiv.css({
-//         'position':'absolute',
-//         'left':posx+'px',
-//         'top':posy+'px',
-//         'display':'none'
-//     }).appendTo( 'body' ).fadeIn(100).delay(1000).fadeOut(500, function(){
-//       $(this).remove();
-//       makeDiv(); 
-//     }); 
-// })();
-
-// function generateRandomForArray() {
-//     var num = Math.floor(Math.random() * 8);
-//     return num;
-// }
-
-// function generateRandom() {
-//     var num = Math.floor(Math.random() * 400);
-//     return num;
-// }
-
-// function showLetter() {
-//     var letter = imgsArray[generateRandomForArray()];
-//     $("div").append("<img src='GameHTML5/images/" + letter + ".png'>");
-//     var left = generateRandom();
-//     var top = generateRandom();
-//     $("div").last().css({"position":"absolute","top": top + "px", "left": left + "px"});
-// }
-
-
-// let canvas = document.querySelector("canvas");
-// let ctx = canvas.getContext("2d");
-
-// function drawCircle(xCenter, yCenter){
-//     ctx.beginPath();
-
-//     ctx.arc(xCenter, yCenter, 3, 0, 2 * Math.PI);
-//     ctx.fill();
-// }
-
-// function generateRandom(minNumber, maxNumber){
-//     let randomDecimal = Math.random(); // generates decimal from 0 to 1 
-//     let range = maxNumber - minNumber;
-//     let ScaleUp = Math.round(randomDecimal * range);
-//     let finalnumber = minNumber + ScaleUp;
-
-//     return finalnumber;
-// }
-
-// function drawRandomCircle(x1, y1, x2, y2){
-//     let xCenter = generateRandom(x1, x2);
-//     let yCenter = generateRandom(y1, y2);
-
-//     drawCircle(xCenter, yCenter);
-// }
-
-// for(var i = 0; i < 5; i++){
-//     drawRandomCircle(5, 5, 200, 100);
-// }
