a84f518c31335d6e5340edea55f66883f1df96ae
Sara-Maged <83797445+Sara-Maged@users.noreply.github.com>
2021-09-15 14:45:20 +0200

    Update game-javascript.js

--- game-javascript.js
+++ game-javascript.js
@@ -8,7 +8,7 @@ function startGame(){
 
     function randomGenerator(){
         for (var i = 0; i < level; i++) {
-            Left = Math.floor(Math.random() * 80) + 10;
+            Left = Math.floor(Math.random() * 75) + 15;
             Top = Math.floor(Math.random() * 80) + 5;
 
             insertionR(Left, Top, i);
@@ -42,7 +42,7 @@ function insertionExtra(iterationExtra){
     $("#R").append("<img src = 'smiley4.png' width='50' height='50' onclick='next()' id=" + iterationExtra + ">");
     return $("#" + iterationExtra).css({
             "position": "absolute",
-                "top": Math.floor(Math.random() * 80) + 10 + "%",
+                "top": Math.floor(Math.random() * 75) + 15 + "%",
                 "left": Math.floor(Math.random() * 80) + 5 + "%"
         });
 };
